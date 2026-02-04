cbb302df5c770cdf35ea97dbaf0ce0d5bec4414e
ShubhamDhamija <shubham.dhamija@juspay.in>
2026-02-04 15:33:02 +0000

    Pull request #2781: fix: XYNE-5689 Fix navigation button colors on call participants screen
    Merge in XYNE/xyne-spaces from fix/call-navigation-issue to main
    
    Squashed commit of the following:
    
    commit b3a1d85db075c197863b8b8acd2df1f21bf91c85
    Author: shubhamdhamija-juspay <shubham.dhamija@juspay.in>
    Date:   Wed Feb 4 18:49:01 2026 +0530
    
        fix: XYNE-5689 Fix navigation button colors on call participants screen
    
        Services-Requiring-Redeployment:
          - dashboard

--- dashboard/src/components/Call/ParticipantGrid/PaginationControls.tsx
+++ dashboard/src/components/Call/ParticipantGrid/PaginationControls.tsx
@@ -30,7 +30,7 @@ export function PaginationControls({
           onClick={onPrevPage}
           className={cn(
             'absolute z-10',
-            'bg-gray-900/80 hover:bg-gray-900 text-white rounded-full',
+            'bg-white hover:bg-gray-100 text-gray-900 rounded-full',
             'transition-all backdrop-blur-sm',
             compact ? 'p-1' : 'p-2',
             vertical ? 'left-1/2 -translate-x-1/2 top-2' : 'left-2 top-1/2 -translate-y-1/2',
@@ -45,7 +45,7 @@ export function PaginationControls({
           onClick={onNextPage}
           className={cn(
             'absolute z-10',
-            'bg-gray-900/80 hover:bg-gray-900 text-white rounded-full',
+            'bg-white hover:bg-gray-100 text-gray-900 rounded-full',
             'transition-all backdrop-blur-sm',
             compact ? 'p-1' : 'p-2',
             vertical ? 'left-1/2 -translate-x-1/2 bottom-2' : 'right-2 top-1/2 -translate-y-1/2',
