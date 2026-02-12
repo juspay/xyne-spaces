00c8176536b34c7a90638aebfdaa65a4acf15f87
Piyush Kesharwani <piyush.k.001@juspay.in>
2026-02-12 12:15:52 +0000

    Pull request #3101: fix: XYNE-6751 stageformModal linting fix
    Merge in XYNE/xyne-spaces from fix/linting-fix-stageformModal to main
    
    Squashed commit of the following:
    
    commit 176f481e50eb1f7a8fa059ce46b8dfa759c161a4
    Author: Piyush Kesharwani <piyush.k.001@juspay.in>
    Date:   Thu Feb 12 17:07:27 2026 +0530
    
        fix: XYNE-6751 stageformModal linting fix
    
        Services-Requiring-Redeployment:
          - dashboard

--- dashboard/src/components/Tickets/StageFormModal/StageFormModal.tsx
+++ dashboard/src/components/Tickets/StageFormModal/StageFormModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useZero } from '@rocicorp/zero/react';
+import { useZero } from '../../../../src/hooks/useZero';
 import { toast } from 'sonner';
 import { v4 as uuidv4 } from 'uuid';
 import { X } from 'lucide-react';
