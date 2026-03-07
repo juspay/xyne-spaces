6c08fc121902080f6d181f08907ed7baa3ad56d7
Sayantan Paul <sayantan.paul@juspay.in>
2026-03-07 10:05:22 +0000

    Pull request #3918: fix: XYNE-10633 Change default landing screen on mobile from Home to DMs
    Merge in XYNE/xyne-spaces from fix/devqa-xyne-mobile-default-landing-dms to main
    
    Squashed commit of the following:
    
    commit b0bb59c41e777bd9a441c637a7e9d1debe46b288
    Author: Sayantan Paul <sayantan.paul@juspay.in>
    Date:   Sat Mar 7 03:33:44 2026 +0530
    
        fix: XYNE-10633 Change default landing screen on mobile from Home to DMs
    
        Services-Requiring-Redeployment:
          - dashboard

--- dashboard/src/routes/HomeScreen/HomeScreen.tsx
+++ dashboard/src/routes/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,17 @@
 import { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import { usePlatform } from '../../hooks/usePlatform';
 
 const HomeScreen = (): ReactElement => {
+  const { isMobile } = usePlatform();
   const { isNewUser } = useAuth();
 
   // If user is new, redirect to onboarding
   if (isNewUser) {
     return <Navigate to='/onboarding' replace />;
   }
-  return <Navigate to='/chat/dir' replace />;
+  return <Navigate to={isMobile ? '/chat/dm' : '/chat/dir'} replace />;
 };
 
 export default HomeScreen;
