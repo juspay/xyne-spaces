43ac7b0b1bbce0d42863fe507361d80ec4087fb1
Archit Narayan <archit.narayan@juspay.in>
2026-04-13 13:17:46 +0000

    Pull request #4965: fix: XYNE-12307 can remove from public also
    Merge in XYNE/xyne-spaces from fix/remove-from-public to main
    
    Squashed commit of the following:
    
    commit d86af0bd6293ae9052b58f8cbd175be6d7508d18
    Author: Archit Narayan <archit.narayan@juspay.in>
    Date:   Mon Apr 13 17:49:39 2026 +0530
    
        fix: XYNE-12307 can remove from public also
    
        Services-Requiring-Redeployment:
          - backend
          - dashboard

--- dashboard/src/zero/mutators.ts
+++ dashboard/src/zero/mutators.ts
@@ -373,21 +373,16 @@ export const mutators = defineMutators({
           throw new Error("Channel doesn't exist");
         }
 
-        // Check if channel is private
-        if (
-          channel.visibility !== ChannelVisibility.PRIVATE ||
-          channel.scopeType !== ChannelScopeType.DEFAULT
-        ) {
-          throw new Error('Participants can only be removed from private channels');
-        }
-
-        // Check if requesting user is a participant (any participant can remove others - Slack default)
         const participationOfRequestingUser = await tx.run(
           zql.channel_participants.where('channelId', channelId).where('userId', ctx.userID).one(),
         );
 
         if (!participationOfRequestingUser) {
-          throw new Error('Only channel members can remove participants from private channels');
+          throw new Error('Only channel members can remove participants');
+        }
+
+        if (participationOfRequestingUser.role !== ChannelRole.ADMIN) {
+          throw new Error('Only channel admins can remove participants');
         }
 
         // Check if target user is actually a participant
