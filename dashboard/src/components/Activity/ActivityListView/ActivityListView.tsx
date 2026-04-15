49cf55bb6a1b42111dd172701401635c34c4162c
Chinmay Singh <chinmay.singh@juspay.in>
2026-04-15 09:39:44 +0000

    Pull request #5062: feat: XYNE-12118 revert activity changes for caching paginated queries
    Merge in XYNE/xyne-spaces from fix/revert-paginated-query-cache to main
    
    Squashed commit of the following:
    
    commit e941fd7517646194b9a3faaccd8541b57a2d21a3
    Author: Chinmay Singh <chinmay.singh@juspay.in>
    Date:   Wed Apr 15 14:07:24 2026 +0530
    
        feat: XYNE-12118 revert activity changes for caching paginated queries
    
        Services-Requiring-Redeployment:
          - dashboard

--- dashboard/src/components/Activity/ActivityListView/ActivityListView.tsx
+++ dashboard/src/components/Activity/ActivityListView/ActivityListView.tsx
@@ -495,7 +495,6 @@ const ActivityListView = (): ReactElement => {
             },
             windowSize: PAGE_SIZE,
             resetKey: [activeTab, currentTypes.join(','), classificationFilter?.join(',') ?? 'all'],
-            cursorEnabled: true,
           }}
           transformItems={transformActivities}
           onItemsChange={handleItemsChange}
