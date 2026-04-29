24a363b1d5d4a29aaac91fbd659127a35f5e2a70
Om Thakur <om.thakur@juspay.in>
2026-04-29 15:45:17 +0000

    Pull request #5598: fix: XYNE-12760: Resolve raw userIds in group DM to channel conversion
    Merge in XYNE/xyne-spaces from fix/local-bot-XYNE-12760 to main
    
    Squashed commit of the following:
    
    commit 0dbbf3c95f3f2b16aa1b051c759826b3f77a27c2
    Author: Abhinav Srivastava <abhinav.s@juspay.in>
    Date:   Wed Apr 29 18:08:15 2026 +0530
    
        fix: XYNE-12760 retain old name
    
        Services-Requiring-Redeployment:
          - backend
    
    commit 2a2a704d7d485137e98ffcc8d3e9e06530ee06fa
    Author: Abhinav Srivastava <abhinav.s@juspay.in>
    Date:   Tue Apr 28 11:12:29 2026 +0530
    
        fix: XYNE-12760 fixx system message in promote DMs channel
    
        Services-Requiring-Redeployment:
          - backend

--- backend/src/zero/side-effects/types.ts
+++ backend/src/zero/side-effects/types.ts
@@ -48,6 +48,7 @@ export interface DelayedMessagePreviousValue {
 
 export interface ChannelPreviousValue {
   name: string;
+  scopeType: string | null;
 }
 
 export interface EmailReadPreviousValue {
