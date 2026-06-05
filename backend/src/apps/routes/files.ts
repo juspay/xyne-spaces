7e8006f406275ddb7fc3aa013b0a6704256d5173
Harimohan Sharma <harimohan.sharma@juspay.in>
2026-06-05 11:31:26 +0000

    Pull request #7141: feat: XYNE-13790 increased date for sbx
    Merge in XYNE/xyne-spaces from feature/XYNE-13790-added-apps-permission2 to main
    
    Squashed commit of the following:
    
    commit 66c5598ed5fb683fec5f89237ee3cd90996d1924
    Author: Harimohan Sharma <harimohan.sharma@juspay.in>
    Date:   Fri Jun 5 15:54:10 2026 +0530
    
        feat: XYNE-13790 increased date for sbx
    
        Services-Requiring-Redeployment:
          - backend

--- backend/src/apps/routes/files.ts
+++ backend/src/apps/routes/files.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { FilesController } from '../controllers/filesController';
 import { validateChannelAccessForPost } from '../middelware/channelValidation';
+import { requirePermission } from '@/middleware/requirePermission';
 
 const router = Router();
 const filesController = new FilesController();
 
-router.post('/filesUpload', validateChannelAccessForPost, filesController.uploadFiles);
-router.get('/info/:attachmentId', filesController.getFileInfo);
-router.get('/download/:attachmentId', filesController.downloadFile);
+router.post('/filesUpload', requirePermission('files:write'), validateChannelAccessForPost, filesController.uploadFiles);
+router.get('/info/:attachmentId', requirePermission('files:read'), filesController.getFileInfo);
+router.get('/download/:attachmentId', requirePermission('files:read'), filesController.downloadFile);
 
 export default router;
