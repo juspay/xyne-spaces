a0846af280831db990f34e7bcaaed0f92e3e3186
Suryansh Srivastava <suryansh.s@juspay.in>
2026-06-23 09:54:22 +0000

    Pull request #7958: fix: XYNE-16423 userGroup related code cleanup
    Merge in XYNE/xyne-spaces from fix/userGroup-code-cleanup to main
    
    Squashed commit of the following:
    
    commit 103c59e033a793acec4fa21da58acab16683b12a
    Author: Suryansh Srivastava <suryansh.s@juspay.in>
    Date:   Tue Jun 23 13:24:56 2026 +0530
    
        fix: XYNE-16423 userGroup related code cleanup
    
        Services-Requiring-Redeployment:
          - dashboard

--- dashboard/src/components/UserGroup/UserManagement/index.tsx
+++ dashboard/src/components/UserGroup/UserManagement/index.tsx
@@ -1,3 +1,2 @@
 export { UserManagement } from './UserManagement/UserManagement';
 export { UserList } from './UserList/UserList';
-export { UserSelector } from './UserSelector/UserSelector';
--- dashboard/src/components/UserGroup/index.ts
+++ dashboard/src/components/UserGroup/index.ts
@@ -1,5 +1,4 @@
-import { UserGroupCard } from './UserGroupCard/UserGroupCard';
 import { UserGroupListItem } from './UserGroupListItem/UserGroupListItem';
 import { UserGroupSidePanel } from './UserGroupSidePanel/UserGroupSidePanel';
 import { OnCallRotationModal } from './OnCallRotationModal/OnCallRotationModal';
-export { UserGroupCard, UserGroupListItem, UserGroupSidePanel, OnCallRotationModal };
+export { UserGroupListItem, UserGroupSidePanel, OnCallRotationModal };
