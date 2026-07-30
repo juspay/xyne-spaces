cfbbd40b6d2f2039458ca3f29f8386c4476b254e
Anurag Dwivedi <anurag.dwivedi@juspay.in>
2026-07-30 16:11:24 +0530

    fast mode

--- packages/xyne-claw-shared/src/index.ts
+++ packages/xyne-claw-shared/src/index.ts
@@ -2,7 +2,7 @@ export type { ToolDefinition, ToolInputSchema, ConfigField, ToolExecutionContext
 export { getAllCustomTools, getCustomTool, getToolsBySource } from "./tools/registry.js";
 export { takeLlmCitations, peekLlmCitations, recordLlmCitations } from "./tools/add-citations/tools.js";
 export { respondToUser, COPILOT_SYSTEM_INSTRUCTION } from "./tools/respond-to-user/index.js";
-export { SUBAGENT_DEFINITIONS, getSubagentDefinition, parseToolsConfig, type SubagentDefinition, type AgentToolsConfig } from "./tools/subagents/index.js";
+export { SUBAGENT_DEFINITIONS, getSubagentDefinition, findSubagentDefinitionForServer, parseToolsConfig, type SubagentDefinition, type AgentToolsConfig } from "./tools/subagents/index.js";
 export { PLATFORM_ONLY_CONFIG_KEYS, stripPlatformConfigKeys } from "./tools/platform-config-keys.js";
 export { getSandboxSession, probeSession, buildSandboxStoreKey, REPO_CONFIGS, SBX_GIT, type RepoSetupConfig, type SetupStep } from "./tools/sandbox/index.js";
 export type { Citation, CitationIconKey } from "./types/citation.js";
