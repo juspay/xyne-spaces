ca1eaae2d0e3cb5b243a354476abc5e536ee2c73
HarimohanSharma11 <harimohan.sharma@juspay.in>
2026-08-13 23:22:20 +0530

    feat: XYNE-56066 Add Pingdom and GCP monitoring support (#556)
    Services-Requiring-Redeployment:
      - backend
      - dashboard

--- packages/shared/src/zero/types.ts
+++ packages/shared/src/zero/types.ts
@@ -465,6 +465,8 @@ export enum AppIncomingWebhookType {
   SLACK = 'SLACK',
   SENTINELONE = 'SENTINELONE',
   AMAZON_SNS = 'AMAZON_SNS',
+  PINGDOM = 'PINGDOM',
+  GCP = 'GCP',
 }
 
 // @ts-ignore TS1294
