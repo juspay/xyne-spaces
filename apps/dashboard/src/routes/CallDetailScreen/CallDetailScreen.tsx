56de7041b69067f8a306290e044277e7f82014a8
Sayantan Paul <sayantan.paul@juspay.in>
2026-09-17 06:00:51 +0000

    feat: XYNE-63011 Calls tab new version (#1674)
    * feat: XYNE-63011 Calls list new version
    
    Services-Requiring-Redeployment:
      - dashboard
    
    * feat: XYNE-63011 list empty state ui
    
    * feat: XYNE-63011 resolve duplication
    
    Services-Requiring-Redeployment:
      - dashboard
    
    * feat: XYNE-63011 cleanup and refactor
    
    Services-Requiring-Redeployment:
      - dashboard
    
    * feat: XYNE-63011 connect calendar sidebar to V2 calls screen
    
    Services-Requiring-Redeployment:
      - dashboard

--- apps/dashboard/src/routes/CallDetailScreen/CallDetailScreen.tsx
+++ apps/dashboard/src/routes/CallDetailScreen/CallDetailScreen.tsx
@@ -264,7 +264,6 @@ export default function CallDetailScreen(): ReactElement {
         aiOpenedRef.current = false;
       }
     });
-    openAI();
     return (): void => {
       subscription.unsubscribe();
       xyneAIActor.send({ type: 'CLOSE' });
