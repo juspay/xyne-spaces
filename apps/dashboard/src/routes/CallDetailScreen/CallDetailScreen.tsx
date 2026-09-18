0b08c5b3a5f173969e396a04010d87181c757f86
Sayantan Paul <sayantan.paul@juspay.in>
2026-09-18 14:25:54 +0000

    Revert "feat: XYNE-63011 Calls tab new version (#1674)" (#2067)
    This reverts commit 56de7041b69067f8a306290e044277e7f82014a8.
    
    Services-Requiring-Redeployment:
      - dashboard

--- apps/dashboard/src/routes/CallDetailScreen/CallDetailScreen.tsx
+++ apps/dashboard/src/routes/CallDetailScreen/CallDetailScreen.tsx
@@ -330,6 +330,7 @@ export default function CallDetailScreen(): ReactElement {
         aiOpenedRef.current = false;
       }
     });
+    openAI();
     return (): void => {
       subscription.unsubscribe();
       xyneAIActor.send({ type: 'CLOSE' });
